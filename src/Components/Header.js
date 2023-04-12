e6e1ac982883f3ef41b9452a507f2873e44b7778
Harsh Pathekar <96983175+pathekarharsh@users.noreply.github.com>
2023-04-12 22:45:17 +0530

    added mobile css also

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.css";
+import { AiOutlineBars } from "react-icons/ai";
 
 function Navbar() {
   return (
     <div className="navbar-container">
       <h1>Inventory/Stock</h1>
+      <div className="side">
+          <AiOutlineBars size={30} style={{ color: "black" }} />
+        </div>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Header from "./Header";
-import Card from './Card';
+import Card from "./Card";
 
 const Home = () => {
   return (
     <div className="major-container">
       <Header />
-          <Card />
+      <div className="C">
+        <Card />
+      </div>
     </div>
   );
 };
